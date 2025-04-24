6ce561823d4eacdb4f0b85cc5376e7bbd3f1aa23
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2025-04-24 19:50:33 -0300

    add unit test for user password hashing on registration

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,4 +1,5 @@
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
 import { RegisterUseCase } from "@/use-cases/register";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
@@ -16,7 +17,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerUseCase = new RegisterUseCase(usersRepository);
     await registerUseCase.execute({ name, email, password });
   } catch (error) {
-    if (error instanceof Error) {
+    if (error instanceof UserAlreadyExistsError) {
       return reply.status(409).send({ message: error.message });
     }
     throw error;
