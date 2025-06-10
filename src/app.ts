39d69274ab6a6346750e0b01f6fccb5486e16f90
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2025-06-09 22:35:40 -0300

    add gym routes tests

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import { usersRoutes } from "./http/controllers/users/routes";
 import { ZodError } from "zod";
 import { env } from "./env";
 import fastifyJwt from "@fastify/jwt";
-import { gymsRoutes } from "./http/controllers/gyms/route";
+import { gymsRoutes } from "./http/controllers/gyms/routes";
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
+
 export const app = fastify();
 
 
@@ -13,6 +15,7 @@ app.register(fastifyJwt, {
 
 app.register(usersRoutes); 
 app.register(gymsRoutes);
+app.register(checkInsRoutes);
 
 
 app.setErrorHandler((error, _, reply) => {
